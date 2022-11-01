a515f8ae7255a9217c0ece6fa982712ebc224b1e
DESKTOP-7J74I5M\LENOVO <sss>
2022-11-01 22:34:33 +0530

    BDD-API Test code

--- src/test/java/stepDefinitions/HttpValidation.java
+++ src/test/java/stepDefinitions/HttpValidation.java
@@ -6,7 +6,6 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class HttpValidation {
     public void verifyGeoResponseMessage(String name) {
-        SerenityRest.restAssuredThat(response -> response.body("country_code", equalTo(country_code))
-                .and().body("name", equalTo(name)));
+        SerenityRest.restAssuredThat(response -> response.body("name", equalTo(name)));
     }
 }
